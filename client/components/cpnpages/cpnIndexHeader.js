3040e89d628e17abfb0adb1ccab4935935bc1933
Jerome Bidault <djedje.thai.ok@gmail.com>
2022-01-23 20:17:02 +0700

    presentation header, sidebar, bannerSlider, done and responssive

--- client/components/cpnpages/cpnIndexHeader.js
+++ client/components/cpnpages/cpnIndexHeader.js
@@ -26,26 +26,28 @@ export default () => {
 				/>
 			</div>
 			<div className="thegrid__presentation-moto">
-				<h3 className="heading-3">Who are we</h3>
-				<h2 className="heading-2 heading-2-light">Best place to buy Toyota</h2>
-				<p className="thegrid__presentation-moto-text">ประสบการณ์งายขายกว่า 10 ปี ช่วยทุกคัน ดันให้ผ่าน ออกได้ทุกอาชีพ คุยกันก่อนได้นะคะ</p>
-				<AccessButton 
-					classname="btn"
-					clicked={() => goToContact()}
-				>
-					contact us
-				</AccessButton>
-			</div>		
-			<div className="thegrid__presentation-owner">
-				<h3 className="heading-3">Top Seller</h3>
-				<DisplayImage 
-					classname=""
-					imgalt="image seller"
-					imgsrc="/images/siang.jpg"
-					imgsrcClassname="thegrid__presentation-owner-image"
-				/>
-				<h4 className="heading-4">Siang</h4>
-				<p className="thegrid__presentation-owner-container-perf">Sold more than 1000 cars</p>
+				<div className="thegrid__presentation-moto1">
+					<h3 className="heading-3">Who are we</h3>
+					<h2 className="heading-2 heading-2-light">Best place to buy Toyota</h2>
+					<p className="thegrid__presentation-moto1-text">ประสบการณ์งายขายกว่า 10 ปี ช่วยทุกคัน ดันให้ผ่าน ออกได้ทุกอาชีพ คุยกันก่อนได้นะคะ</p>
+					<AccessButton 
+						classname="btn"
+						clicked={() => goToContact()}
+					>
+						contact us
+					</AccessButton>
+				</div>		
+				<div className="thegrid__presentation-moto2">
+					<h3 className="heading-3">Top Seller</h3>
+					<DisplayImage 
+						classname=""
+						imgalt="image seller"
+						imgsrc="/images/siang.jpg"
+						imgsrcClassname="thegrid__presentation-moto2-image"
+					/>
+					<h4 className="heading-4">Siang</h4>
+					<p className="thegrid__presentation-owner-container-perf">Sold more than 1000 cars</p>
+				</div>
 			</div>
 		</div>
 	)
