9b1ae236f31a919de705b1033c6667218074790a
Jerome Bidault <djedje.thai.ok@gmail.com>
2022-01-29 17:26:15 +0700

    before adding babelrc

--- client/components/cards/cardStd.js
+++ client/components/cards/cardStd.js
@@ -2,15 +2,36 @@ import React from 'react'
 
 const CardStd = (props) => {
 
-	// <img src={props.carDetails.pic} style={{width:150}} /><br />
+	// <img src={props.carDetails.pic} style={{width:300}} /><br />
 
 	return(
-		<div onClick={props.clicked}>
-			<img src={props.carDetails.pic} style={{width:300}}/><br />
-			{props.carDetails.serie}<br/>
-			{props.carDetails.price}<br/>
-			{props.carDetails.style}<br/>
-			{props.carDetails.engine}<br/>		
+		<div className="standart" onClick={props.clicked}>
+			<img src={props.carDetails.pic} className="standart__img" /><br />
+			<h5 className="standart__serie">{props.carDetails.serie}</h5>
+			<div className="standart__style">
+				<svg>
+				<use xlink:href="/svg/sprite.svg#icon-shield"></use>
+				</svg>
+				<p>{props.carDetails.style}</p>
+			</div>
+			<div className="standart__grade">
+				<svg>
+				<use xlink:href="/svg/sprite.svg#icon-trophy"></use>
+				</svg>
+				<p>{props.carDetails.grade}</p>
+			</div>
+			<div className="standart__engine">
+				<svg>
+				<use xlink:href="/svg/sprite.svg#icon-gauge"></use>
+				</svg>
+				<p>{props.carDetails.engine}</p>
+			</div>
+			<div className="standart__price">
+				<svg>
+				<use xlink:href="/svg/sprite.svg#icon-credit-card"></use>
+				</svg>
+				<p>{props.carDetails.price}</p>
+			</div>
 		</div>
 	)
 }
