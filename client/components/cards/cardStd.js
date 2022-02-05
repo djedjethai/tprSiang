e77e4f436970bbd116a18cacbf1fd3dd33ae6a60
Jerome Bidault <djedje.thai.ok@gmail.com>
2022-02-05 17:27:26 +0700

    cardStd done and headerStyle selected main card done

--- client/components/cards/cardStd.js
+++ client/components/cards/cardStd.js
@@ -10,20 +10,20 @@ const CardStd = (props) => {
 			<img src={props.carDetails.pic} className="standart__img" />
 			<h5 className="standart__serie">{props.carDetails.serie}</h5>
 			<div className="standart__style">
-				<span className="material-icons">bookmark</span>
+				<span className="material-icons standart__icons">bookmark</span>
 				<p>{props.carDetails.style}</p>
 			</div>
 			<div className="standart__grade">
-				<span className="material-icons">loyalty</span>
+				<span className="material-icons standart__icons">loyalty</span>
 				<p>{props.carDetails.grade}</p>
 			</div>
 			<div className="standart__engine">
-				<span className="material-icons">build</span>
+				<span className="material-icons standart__icons ">build</span>
 				<p>{props.carDetails.engine}</p>
 			</div>
 			<div className="standart__price">
-				<span className="material-icons">credit_card</span>
-				<p>{props.carDetails.price} Thb</p>
+				<span className="material-icons standart__icons">credit_card</span>
+				<p>{props.carDetails.price} thb</p>
 			</div>
 		</div>
 	)
--- client/components/cards/headerStyle.js
+++ client/components/cards/headerStyle.js
@@ -3,12 +3,27 @@ import React from 'react'
 const HeaderStyle = (props) => {
 
 	return(
-		<div onClick={props.clicked}>
-			<img src={props.carDetails.pic} style={{width:300}} /><br />
-			{props.carDetails.serie}<br/>
-			{props.carDetails.price}<br/>
-			{props.carDetails.style}<br/>
-			{props.carDetails.engine}<br/>		
+		<div className="style" onClick={props.clicked}>
+			<img src={props.carDetails.pic} className="style__img" /><br />
+			<h2 className="heading-2 style__choiced">{props.carDetails.serie}</h2>
+			<div className="style__info">
+				<div>
+					<span className="material-icons style__icons">bookmark</span>
+					<h4 className="heading-4 style__caracteristique">{props.carDetails.style}</h4>
+				</div>
+				<div>
+					<span className="material-icons style__icons">loyalty</span>
+					<h4 className="heading-4 style__caracteristique">{props.carDetails.grade}</h4>
+				</div>
+				<div>
+					<span className="material-icons style__icons">build</span>
+					<h4 className="heading-4 style__caracteristique">{props.carDetails.engine}</h4>
+				</div>
+				<div>
+					<span className="material-icons style__icons">credit_card</span>
+					<h4 className="heading-4 style__caracteristique">{props.carDetails.price} Thb</h4>
+				</div>
+			</div>
 		</div>
 	)
 }
